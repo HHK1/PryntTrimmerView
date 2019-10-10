87d0233b33bd6ea9c9582a61b6d440614f2f7ada
Theodor Müller <info@trilliarden.net>
2019-10-10 18:02:05 +0200

    Expose maskColor as property

--- PryntTrimmerView/Classes/Trimmer/PryntTrimmerView.swift
+++ PryntTrimmerView/Classes/Trimmer/PryntTrimmerView.swift
@@ -45,6 +45,14 @@ public protocol TrimmerViewDelegate: class {
             positionBar.backgroundColor = positionBarColor
         }
     }
+  
+    /// The color used to mask unselected parts of the video
+    @IBInspectable public var maskColor: UIColor = UIColor.white {
+        didSet {
+            leftMaskView.backgroundColor = maskColor
+            rightMaskView.backgroundColor = maskColor
+        }
+    }
 
     // MARK: Interface
 
