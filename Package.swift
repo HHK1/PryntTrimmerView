d59a1639bfae4e13a1d551f97bb8f6a46b3016eb
HHK1 <henryhuck@hotmail.fr>
2020-07-14 18:56:49 +0200

    attempt to hide some files

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "PryntTrimmerView")
+            name: "PryntTrimmerView",
+            exclude: ["PryntTrimmerViewExample", "LICENSE"])
     ],
     swiftLanguageVersions: [SwiftVersion.v5]
 )
